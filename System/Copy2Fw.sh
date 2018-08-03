39a1934891deeaaab2a1e9d257e4fb1f146c24e3
Marcos Machado <marcosmachado@gmail.com>
2018-08-02 18:44:51 -0700

    Defining vars for FW access

--- System/Copy2Fw.sh
+++ System/Copy2Fw.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-FW="10.255.255.1"
-PORT="50022"
+if [ -z ${UTMWEBTEMPLATES_FWADDR} ]; then
+  echo "Please define the ENV var UTMWEBTEMPLATES_FWADDR with your UTM address"
+  exit
+fi
 
+if [ -z ${UTMWEBTEMPLATES_SSHPORT} ]; then
+  echo "Please define the ENV var UTMWEBTEMPLATES_SSHPORT with your UTM SSH port"
+  exit
+fi
 
+FW=${UTMWEBTEMPLATES_FWADDR}
+PORT=${UTMWEBTEMPLATES_SSHPORT}
+
+echo Fw: ${FW}
+echo Port: ${PORT}
